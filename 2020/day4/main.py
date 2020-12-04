af495b72ba72be9ed88e5eb301feda6dd29c3c15
Filip Doversten <filip@doversten.se>
2020-12-04 08:21:23 +0100

    Cleaned up commented-code and an assert for debug on day4

--- 2020/day4/main.py
+++ 2020/day4/main.py
@@ -16,15 +16,8 @@ def validate_pp_p2(pp):
     eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
     props = re.findall(r"(?:^|\n| )(\w+):([\w\d#]+)(?=$|\n| )", pp)
-    #print("basdf")
-    #print(props)
-    #names,vals = zip(*props)
-    #print(names)
-    #print(vals)
 
     for n,v in props:
-        #print("--n={} v={}".format(n,v))
-
         if n == "byr" and 1920 <= int(v) <= 2002:
             print("valid byr: " + v+" (1920-2002)")
             valid_props["byr"] = True
@@ -38,7 +31,6 @@ def validate_pp_p2(pp):
             mtch = re.match(r"(\d+)(in|cm)", v)
             if mtch != None:
                 val,unit = mtch.groups()
-                #print("val={} unit={}".format(val,unit))
                 if (unit == "cm" and 150 <= int(val) <= 193) or (unit == "in" and 59 <= int(val) <= 76):
                     print("valid hgt: {}.{} (cm: 150-193, in: 59-76)".format(val,unit))
                     valid_props["hgt"] = True
@@ -51,22 +43,7 @@ def validate_pp_p2(pp):
         elif n == "pid" and re.match(r"^\d{9}$", v) != None:
             print("valid pid: "+v+" count="+str(len(v)))
             valid_props["pid"] = True
-            if (len(v) != 9):
-                assert(False)
-        #else:
-        #    if n == "byr":
-        #        print("invalid {}: {} (1920-2002)".format(n,v))
-        #    elif n == "iyr":
-        #        print("invalid {}: {} (2010-2020)".format(n,v))
-        #    elif n == "eyr":
-        #        print("invalid {}: {} (2020-2030)".format(n,v))
-        #    elif n == "ecl":
-        #        print("invalid {}: {} {}".format(n,v,eye_colors))
-        #    elif n == "hgt":
-        #        print("invalid {}: {} (cm 150-193, in 59-76)".format(n,v))
-        #    else:    
-        #        print("invalid {}: {}".format(n,v))
-    
+
     print("valid_props.values = " + str(valid_props.values()))
     return all(valid_props.values())
 
@@ -79,6 +56,5 @@ for pp in pps:
     if valid:
         valid_count += 1
     print("------ " + str(valid))
-    #input("press enter")
 
 print("valid count = " + str(valid_count))
